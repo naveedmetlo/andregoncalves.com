c19b6d876734171a6a09ad16ee2848a9039213fd
Andre Goncalves <andre@questionform.com>
2011-07-13 19:33:40 +0100

    Blog titles and analytics page speed
    Added blog titles do meta title tag
    Added Ganalytics page speed tracking

--- site/js/application.js
+++ site/js/application.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', "UA-212686-7"]);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_trackPageLoadTime']);
+
   $("#sidebar a").bind('click', function() {
     var location = $(this).attr('name');
     if (window._gaq) _gaq.push(['_trackEvent', 'Sidebar', 'Link', location]);
@@ -14,4 +19,6 @@ $(document).ready(function() {
       _gaq.push(['_trackSocial', 'twitter', 'tweet', targetUrl]);
     }
   });
+
+
 });
