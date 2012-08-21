0b75b5ba7fa85579ff945c193e669cf56693e026
Andre Goncalves <andre@questionform.com>
2012-08-21 11:25:34 +0100

    Added code canyon projects example page, css improvements

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,41 @@
+#############################################################
+#	Application
+#############################################################
 set :application,       'home'
+set :host,              'andregoncalves.com'
+
+
+ssh_options[:port] = 8761
+default_run_options[:pty] = true
+
+#############################################################
+#	Source Control Settings
+#############################################################
 set :repository,        'site/_site'
 set :scm,               :none
 set :deploy_via,        :copy
 set :copy_compression,  :gzip
 #set :use_sudo,          true
-set :host,              'andregoncalves.com'
 
-ssh_options[:port] = 8761
-
+#############################################################
+#	Roles
+#############################################################
 role :web,  host
 role :app,  host
 role :db,   host, :primary => true
 
+
+#############################################################
+#	Ownership
+#############################################################
 set :user,    'andregoncalves'
 set :group,   user
 
-#set(:dest) { Capistrano::CLI.ui.ask("Destination: ") }
-
-#if dest == 'dev'
-#  set :deploy_to,    "/home/#{user}/sites/dev.#{application}.com"
-#elsif dest == 'www'
-#  set :deploy_to,    "/home/#{user}/sites/#{application}.com"
-#end
 
 set :deploy_to,    "/var/sites/#{application}"
 
 before 'deploy:update', 'deploy:jekyll'
+after 'deploy:update', 'deploy:change_ownership'
 
 namespace :deploy do
   [:start, :stop, :restart, :finalize_update].each do |t|
@@ -36,5 +46,10 @@ namespace :deploy do
   desc 'Run jekyll to update site before uploading'
   task :jekyll do
     #%x(rm -rf _site/* && jekyll)
+  end
+
+  desc 'Change ownership'
+  task :change_ownership do
+    sudo "chown -R www-data:www-data #{release_path}"
   end
 end
